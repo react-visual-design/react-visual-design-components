4eed0c025e43f4a2db0dac34fe038ef3968f2b20
zhaogh <zhaogh@xiaoshouyi.com>
2020-12-25 18:48:24 +0800

    feat: eslint husky  fabric config

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  extends: [require.resolve('@umijs/fabric/dist/eslint')],
+
+  globals: {},
+  parser: 'babel-eslint',
+  rules: {
+    semi: [2, 'never'],
+    'import/no-extraneous-dependencies': 0,
+    'prefer-destructuring': 0,
+    'no-return-assign': 0,
+    'no-param-reassign': 0,
+    'jsx-a11y/alt-text': 0,
+    'react/jsx-uses-react': 2,
+  },
+}
---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# 1.0.0 (2020-12-25)
+
+### Features
+
+- dumi init ([01bbc34](https://github.com/kokiy/react-h5-calendar/commit/01bbc34439970d47debefc68956166bef8c8a9e2))
