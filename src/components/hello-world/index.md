e09b24146d62cbb9a73f3cb47bf8df273ee0d865
koki <2788293436@qq.com>
2020-12-25 19:00:56 +0800

    feat: 新增日历组件

---
+++ src/components/hello-world/index.md
@@ -0,0 +1,10 @@
+## Hello
+
+Demo:
+
+```jsx
+import React from 'react'
+import { HelloWorld } from 'zimi-components'
+
+export default () => <HelloWorld title="hello world" />
+```
--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
-export { default as Foo } from './Foo'
+import 'normalize.css'
+
+export { default as HelloWorld } from './components/hello-world'
+export { default as Calendar } from './components/calendar'
