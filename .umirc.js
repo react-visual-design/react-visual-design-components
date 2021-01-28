a777827a1a4084415e490d04e59ff7ca7857bd38
koki <2788293436@qq.com>
2021-01-28 16:12:47 +0800

    fix:  doc build & demo

--- .umirc.js
+++ .umirc.js
@@ -9,6 +9,14 @@ export default defineConfig({
     'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
   outputPath: 'docs-dist',
   hash: true,
+  publicPath: '/react-visual-design-components/',
+  base: '/react-visual-design-components/',
+  themeConfig: {
+    hd: {
+      rules: [], // 禁用高清方案
+      // 更多 rule 配置访问 https://github.com/umijs/dumi/blob/master/packages/theme-mobile/src/typings/config.d.ts#L7
+    },
+  },
   extraBabelPlugins: [
     [
       'babel-plugin-import',
